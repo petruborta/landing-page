957f5cfb36a8393a1720d9c2b0e28590aa2b8a14
petruborta <petruborta@yahoo.com>
2021-02-13 02:00:53 +0000

    Initial commit Added content to page Embedded a custom map using Google Maps JS API

---
+++ js/map.js
@@ -0,0 +1,117 @@
+function initMap() {
+  const hotPizza = { lat: 51.505749609938576, lng: -0.11510476562161634 };
+  const map = new google.maps.Map(document.getElementsByClassName("map")[0], {
+    zoom: 15,
+    center: hotPizza,
+    styles: [
+      { "elementType": "geometry", "stylers": [{ "color": "#212121"}]},
+      { "elementType": "labels.icon", "stylers": [{ "visibility": "off"}]},
+      { "elementType": "labels.text.fill", "stylers": [{ "color": "#757575"}]},
+      { "elementType": "labels.text.stroke", "stylers": [{ "color": "#212121"}]},
+      {
+        "featureType": "administrative", 
+        "elementType": "geometry", 
+        "stylers": [{ "color": "#757575"}]
+      },
+      {
+        "featureType": "administrative.country", 
+        "elementType": "labels.text.fill", 
+        "stylers": [{ "color": "#9e9e9e"}]
+      },
+      {
+        "featureType": "administrative.land_parcel", 
+        "stylers": [{ "visibility": "off"}]
+      },
+      {
+        "featureType": "administrative.locality", 
+        "elementType": "labels.text.fill", 
+        "stylers": [{ "color": "#bdbdbd"}]
+      },
+      {
+        "featureType": "poi", 
+        "elementType": "geometry.fill", 
+        "stylers": [{ "color": "#713524"}]
+      },
+      {
+        "featureType": "poi", 
+        "elementType": "labels.icon", 
+        "stylers": [{ "visibility": "on"}]
+      },
+      {
+        "featureType": "poi.park", 
+        "elementType": "geometry", 
+        "stylers": [{ "color": "#181818"}]
+      },
+      {
+        "featureType": "poi.park", 
+        "elementType": "geometry.fill", 
+        "stylers": [{ "color": "#713524"}]
+      },
+      {
+        "featureType": "road", 
+        "elementType": "geometry.fill", 
+        "stylers": [{ "color": "#2c2c2c"}]
+      },
+      {
+        "featureType": "road", 
+        "elementType": "labels.text.fill", 
+        "stylers": [{ "color": "#8a8a8a"}]
+      },
+      {
+        "featureType": "road.arterial", 
+        "elementType": "geometry", 
+        "stylers": [{ "color": "#373737"}]
+      },
+      {
+        "featureType": "road.arterial", 
+        "elementType": "geometry.fill", 
+        "stylers": [{ "color": "#b13816"}]
+      },
+      {
+        "featureType": "road.highway", 
+        "elementType": "geometry", 
+        "stylers": [{ "color": "#3c3c3c"}]
+      },
+      {
+        "featureType": "road.highway", 
+        "elementType": "geometry.fill", 
+        "stylers": [{ "color": "#b13816"}]
+      },
+      {
+        "featureType": "transit", 
+        "elementType": "labels.text.fill", 
+        "stylers": [{ "color": "#757575"}]
+      },
+      {
+        "featureType": "water", 
+        "elementType": "geometry", 
+        "stylers": [{ "color": "#000000"}]
+      },
+      {
+        "featureType": "water", 
+        "elementType": "labels.text.fill", 
+        "stylers": [{ "color": "#3d3d3d"}]
+       }
+    ]
+  });
+  const contentString =
+    '<div class="info-window">' +
+      '<h1 class="info-window-heading">HOTPIZZA</h1>' +
+      '<div class="info-window-content">' +
+        "<p>Unit 3, The White House<br/>9c Belvedere Road, South Bank<br/>London SE1 8YP</p>" +
+      "</div>" +
+    "</div>";
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+  });
+  const marker = new google.maps.Marker({
+    position: hotPizza,
+    map: map,
+    title: "Pizzeria HotPizza",
+  });
+  marker.addListener("click", () => {
+    infowindow.open(map, marker);
+  });
+
+  infowindow.open(map, marker);
+}

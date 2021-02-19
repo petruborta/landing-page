e0263a3a19406a5b23c17986880e5d70efba8e20
petruborta <petruborta@yahoo.com>
2021-02-19 23:49:01 +0000

    Added animation to navbar and hero section

---
+++ js/main.js
@@ -0,0 +1,31 @@
+let scroll = window.requestAnimationFrame ||
+  function(callback){ window.setTimeout(callback, 1000/60)};
+
+let elementsToShow = document.querySelectorAll(".show-on-scroll");
+
+function loop() {
+  elementsToShow.forEach((element) => {
+    if (isElementInViewport(element)) {
+      element.classList.add("is-visible");
+    } else {
+      element.classList.remove("is-visible");
+    }
+  });
+
+  scroll(loop);
+}
+
+function isElementInViewport(el) {
+  let  rect = el.getBoundingClientRect();
+  
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    || (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    || (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
+
+loop();
